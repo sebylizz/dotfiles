de85319cec43acaee0426d3bf580240b21af5c3c
sebylizz <sebastiankjandersen@gmail.com>
2024-05-21 15:56:39 +0200

    clipboard fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,13 @@
 require("seby")
+vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+        ["+"] = 'clip.exe',
+        ["*"] = 'clip.exe',
+    },
+    paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+}
