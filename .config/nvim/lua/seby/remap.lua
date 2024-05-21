7248210da5cfd99dd0fbf3004db7a3dc2b9a497a
Sebastian <sebastiankjandersen@gmail.com>
2024-05-21 22:38:04 +0200

    move

--- .config/nvim/lua/seby/remap.lua
+++ .config/nvim/lua/seby/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<leader>y", "\"+y")
